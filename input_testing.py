38683838396ea3af5ede3b26757c6775f21619eb
aevdo <a.evdokimov135@mail.ru>
2022-10-20 14:17:00 +0300

    smol upgr

--- input_testing.py
+++ input_testing.py
@@ -1,24 +1,29 @@
-from dict import ENG_TO_RU_DICT, RU_TO_ENG_DICT
+from dict import *
 
-lang = input('Choose the needed lang ("en" or "ru"): ')
-output = ''
 
-if lang == 'ru':
-    user_input = input('Initial text: ')
-    for letter in user_input:
-        if letter in ENG_TO_RU_DICT:
-            output += ENG_TO_RU_DICT[letter]
-        else:
-            output += letter
-    print(output)
+def letters_transform(lang='ru', user_input=''):
+    output = ''
+    if lang == 'ru':
+        # user_input = input('Initial text: ')
+        for letter in user_input:
+            if letter in ENG_TO_RU_DICT:
+                output += ENG_TO_RU_DICT[letter]
+            else:
+                output += letter
+        return output
 
-elif lang == 'en':
-    user_input = input('Initial text: ')
-    for letter in user_input:
-        if letter in RU_TO_ENG_DICT:
-            output += RU_TO_ENG_DICT[letter]
-        else:
-            output += letter
-    print(output)
+    elif lang == 'en':
+        # user_input = input('Initial text: ')
+        for letter in user_input:
+            if letter in RU_TO_ENG_DICT:
+                output += RU_TO_ENG_DICT[letter]
+            else:
+                output += letter
+        return output
 
-else: print(f"I don't know the language {lang}, try again")
+    else: print(f"I don't know the language {lang}, try again")
+
+
+language = input('Choose the needed lang ("en" or "ru"): ')
+user_in = input('Initial text: ')
+letters_transform(language, user_in)
