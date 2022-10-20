bdca684bfd7dc3adab58cba6350468a745867829
aevdo <a.evdokimov135@mail.ru>
2022-10-20 15:49:55 +0300

    Basic bot ready

---
+++ translit_bot.py
@@ -0,0 +1,19 @@
+# https://habr.com/ru/post/442800/
+
+import telebot
+from input_testing import letters_transform
+bot = telebot.TeleBot('5736580585:AAHTmzjKZTfukU3eKt4FdQJEQs1zsKhB3aU')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "/start":
+        bot.send_message(message.from_user.id, "Привет, я переделываю сообщения из латинской в русскую раскладку")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Сообщение должно быть на латинской раскладке. "
+                                               "Пришли его мне и я его переделаю")
+    else:
+        bot.send_message(message.from_user.id, letters_transform(user_input=message))
+
+
+bot.polling(none_stop=True, interval=0)
