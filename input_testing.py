f8361626ec94cde814d1ba91cd503c327880c47b
alexander <a.evdokimov135@mail.ru>
2022-10-21 00:14:32 +0300

    Хорошенько так пофиксил. Теперь любой символ переводится в противооложную раскладку

--- input_testing.py
+++ input_testing.py
@@ -1,28 +1,18 @@
-from dict import *
+from dict import RU_TO_ENG_DICT, ENG_TO_RU_DICT
 
 
 def letters_transform(user_input):
     output = ''
-    if user_input[0] in ENG_TO_RU_DICT:
-        for letter in user_input:
-            if letter in ENG_TO_RU_DICT:
-                output += ENG_TO_RU_DICT[letter]
-            else:
-                output += letter
-        return output
-
-    elif user_input[0] in RU_TO_ENG_DICT:
-        for letter in user_input:
-            if letter in RU_TO_ENG_DICT:
-                output += RU_TO_ENG_DICT[letter]
-            else:
-                output += letter
-        return output
-
-    else:
-        return user_input
+    for letter in user_input:
+        if letter in ENG_TO_RU_DICT:
+            output += ENG_TO_RU_DICT[letter]
+        elif letter in RU_TO_ENG_DICT:
+            output += RU_TO_ENG_DICT[letter]
+        else:
+            output += letter
+    return output
 
 
-# language = 'ru'
-# user_in = 'привет z ntcnjdsq cjj,otybt lkz <jnf ^))'
-# print(letters_transform(user_input=user_in))
+if __name__ == '__main__':
+    user_in = 'привет z ntcnjdsq cjj,otybt lkz <jnf ^))'
+    print(letters_transform(user_input=user_in))
--- translit_bot.py
+++ translit_bot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot('5704603308:AAHVM2f5_BGKg94-jbQbKxZX9rSVk2LeYyo')  # https
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    print(f'Message received from {message.from_user.id}: {message.text}')
+    print(f'User {message.from_user.id}: {message.text}')
     if message.text == "/start":
         # send greetings
         bot.send_message(message.from_user.id, "Привет, я переделываю сообщения в другую раскладку. "
@@ -17,16 +17,15 @@ def get_text_messages(message):
     elif message.text == "/help":
         # show bot description
         bot.send_message(message.from_user.id, "Присылай любое сообщение чтобы я его переделал в другую "
-                         "раскладку\nИз русской в английскую переводить или наоборот - я определяю "
-                         "по первой букве. Поэтому если напишешь первый символ которого нет в моей базе (например, "
-                         "'!' или число, вряд ли я выведу то, что ты ожидаешь")
+                         "раскладку\nПеревожу из русской в английскую раскладку и наоборот. Работает для традиционной "
+                         "раскладки ПК, на Mac обычно всё кроме 'ё' и некоторых символов в других местах(")
         # log the message to python console
-        print(f'Answer to {message.from_user.id}: "Присылай любое сообщение чтобы я его переделал в другую раскладку\n'
-              f'Из русской в английскую или наоборот я определяю по первой букве. Поэтому если напишешь одно сообщение '
-              f'на разных раскладках, вряд ли я выведу точно то, что ты ожидаешь"')
+        print(f'Answer to {message.from_user.id}: Присылай любое сообщение чтобы я его переделал в другую раскладку\n'
+              f'Перевожу из русской в английскую раскладку и наоборот. Работает для традиционной раскладки ПК, '
+              f"на Mac обычно всё кроме 'ё' и некоторых символов в других местах(")
 
     else:
-        # reply with the changed message
+        # reply with the changed text
         bot.send_message(message.from_user.id, letters_transform(user_input=message.text))
 
         # log the message to python console
