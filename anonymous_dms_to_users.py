668c109d77fd7d25db93f29bc620ad811428c588
alexander <a.evdokimov135@mail.ru>
2022-10-21 17:28:17 +0300

    поправил анонимус мессенджер ахаххаххах

--- anonymous_dms_to_users.py
+++ anonymous_dms_to_users.py
@@ -1,6 +1,8 @@
 import telebot
 from unittest import TestCase
-bot = telebot.TeleBot('5704603308:AAHVM2f5_BGKg94-jbQbKxZX9rSVk2LeYyo')
+bot = telebot.TeleBot('5765608273:AAHZ7gIWuGttGJ-0Ch7J6C0W_v_bEt7Pw_0')
+# 5765608273:AAHZ7gIWuGttGJ-0Ch7J6C0W_v_bEt7Pw_0
+# 5704603308:AAHVM2f5_BGKg94-jbQbKxZX9rSVk2LeYyo
 
 
 class TestSendMessage(TestCase):
@@ -11,6 +13,8 @@ class TestSendMessage(TestCase):
         @bot.message_handler(content_types=['text'])
         def get_text_messages(message):
             print(f'{message.from_user.id}: {message.text}')
+            a = input()
+            bot.send_message(int('689585726'), a)
 
         bot.polling(none_stop=True, interval=0)
 
@@ -21,6 +25,8 @@ class TestSendMessage(TestCase):
         @bot.message_handler(content_types=['text'])
         def get_text_messages(message):
             print(f'{message.from_user.id}: {message.text}')
+            a = input()
+            bot.send_message(int('1840388068'), a)
 
         bot.polling(none_stop=True, interval=0)
 
